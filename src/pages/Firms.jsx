c9b8bd2f0d4eadd7f0e5facb3e6ee31f8bedbb4f
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-04-03 22:30:48 +0300

    usestockcall added

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,7 +1,38 @@
 import { Button, Typography } from "@mui/material"
+import axios from "axios"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice"
+import useStockCall from "../hooks/useStockCall"
 
 
 const Firms = () => {
+  const {getStockData} =useStockCall()
+  // const { token } = useSelector((state) => state.auth)
+  // const dispatch = useDispatch()
+
+  // const getFirms = async () => {
+  //   const BASE_URL = "https://12125.fullstack.clarusway.com/"
+  //   dispatch(fetchStart())
+  //   const url = "firms"
+  //   try {
+  //     const { data } = await axios(`${BASE_URL}stock/firms/`, {
+  //       headers: { Authorization: `Token ${token}` },
+  //     })
+  //     dispatch(getSuccess({ data, url }))
+  //   } catch (error) {
+  //     console.log(error)
+  //     dispatch(fetchFail())
+  //   }
+  // }
+
+  useEffect(() => {
+    
+  // getFirms()
+  getStockData("firms")
+    
+  }, [])
+  
   return (
     <div>
       <Typography variant="h4" color="error" mb={3}>
