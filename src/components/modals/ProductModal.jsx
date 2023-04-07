21418cac2b7e5881b437cb3a8c75909083b6e159
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-04-07 15:00:43 +0300

    product added

---
+++ src/components/modals/ProductModal.jsx
@@ -0,0 +1,116 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { modalStyle } from '../../styles/globalStyle';
+import { TextField } from '@mui/material';
+import { useState } from 'react';
+import useStockCall from '../../hooks/useStockCall';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function ProductModal({open,handleClose ,info ,setInfo}) {
+    // const [info, setInfo] = useState({
+    //     name:"",
+    //     phone:"",
+    //     address:"",
+    //     image:"",
+    // })
+    const {postStockData} = useStockCall()
+    const {putStockData} = useStockCall()
+
+ const handleChange=(e)=>{
+    const { name, value } = e.target
+    setInfo({ ...info, [name]: value })
+ }
+
+ const handleSubmit=(e)=>{
+  e.preventDefault();
+  if(info.id){
+    putStockData("products" , info)
+  }else{
+      postStockData("products" , info)
+  }
+
+  handleClose()
+  setInfo({name:"",phone:"",address:"",image:""})
+ }
+
+  return (
+    <div>
+      
+      <Modal
+        open={open}
+        onClose={()=>{
+          handleClose()
+          setInfo({name:"",phone:"",address:"",image:""})
+        }}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+         component="form"
+          onSubmit={handleSubmit}>
+             <TextField
+               label="Firm Name"
+               name="name"
+               id="name"
+               type="text"
+               required
+               variant="outlined"
+               value={info?.name}
+               onChange={handleChange}
+            />  
+             <TextField
+               label="Phone"
+               name="phone"
+               id="phone"
+               type="tel"
+               required
+               variant="outlined"
+               value={info?.phone}
+               onChange={handleChange}
+            />  
+             <TextField
+               label="Address"
+               name="address"
+               id="address"
+               type="text"
+               required
+               variant="outlined"
+               value={info?.address}
+               onChange={handleChange}
+            />  
+             <TextField
+               label="Image"
+               name="image"
+               id="image"
+               type="url"
+               required
+               variant="outlined"
+               value={info?.image}
+               onChange={handleChange}
+            />  
+
+               <Button type="submit" variant="contained" >
+              Submit Product
+            </Button>
+
+             </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,71 @@
 
+import { Button, Grid, Typography } from "@mui/material"
+import axios from "axios"
+import { useEffect,useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice"
+import useStockCall from "../hooks/useStockCall"
+import ProductCard from "../components/ProductCard"
+import ProductModal from "../components/modals/ProductModal"
+
 
 const Products = () => {
+  const {getStockData} =useStockCall()
+  const {firms} = useSelector((state)=>state.stock)
+  const [open, setOpen] = useState(false);
+  const [info, setInfo] = useState({
+    name:"",
+    phone:"",
+    address:"",
+    image:"",})
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  // const { token } = useSelector((state) => state.auth)
+  // const dispatch = useDispatch()
+
+  // const getFirms = async () => {
+  //   const BASE_URL = "https://12125.fullstack.clarusway.com/"
+  //   dispatch(fetchStart())
+  //   const url = "firms"
+  //   try {
+  //     const { data } = await axios(`${BASE_URL}stock/firms/`, {
+  //       headers: { Authorization: `Token ${token}` },
+  //     })
+  //     dispatch(getSuccess({ data, url }))
+  //   } catch (error) {
+  //     console.log(error)
+  //     dispatch(fetchFail())
+  //   }
+  // }
+
+  useEffect(() => {
+    
+  // getFirms()
+  getStockData("products")
+    
+  }, [])
+  
   return (
     <div>
-      products
+      <Typography variant="h4" color="error" mb={3}>
+        Products
+      </Typography>
+      <Button variant="contained" onClick={handleOpen}>New Product</Button>
+
+      <ProductModal open={open} handleClose={handleClose} info={info} setInfo={setInfo}/>
+<Grid container  >
+  {firms?.map((firm)=>(
+    <Grid item key={firm.id}>
+      <ProductCard firm={firm} setOpen={setOpen} info={info} setInfo={setInfo}/>
+
+    </Grid>
+  ))}
+
+</Grid>
+
     </div>
   )
 }
 
 export default Products
+
