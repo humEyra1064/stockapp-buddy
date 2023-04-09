f423b603a898bb786749892725a1ecb527f73998
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-04-09 23:15:32 +0300

    home added

---
+++ src/components/KpiCards.jsx
@@ -0,0 +1,26 @@
+import { Grid } from "@mui/material"
+
+
+const KpiCards = () => {
+    const data =[
+        {id:1,
+            title:"sales",
+            value:"$2000",
+            
+
+        
+        }
+
+
+    ]
+  return (
+    <Grid container>
+        <Grid item>
+
+        </Grid>
+      
+    </Grid>
+  )
+}
+
+export default KpiCards
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Typography } from "@mui/material"
+import { useEffect } from "react"
+import Charts from "../components/Charts"
+import KpiCards from "../components/KpiCards"
+import useStockCall from "../hooks/useStockCall"
 
 const Home = () => {
+  const { getStockData } = useStockCall()
+
+  useEffect(() => {
+    getStockData("sales")
+    getStockData("purchases")
+  }, [])
+
   return (
     <div>
-      home
+      <Typography variant="h4" color="error" mb={3}>
+        Dashboard
+      </Typography>
+
+      <KpiCards />
+      <Charts />
     </div>
   )
 }
